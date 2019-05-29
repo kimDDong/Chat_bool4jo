f9dd8c7320edbb74d925d2e3bd7c1800b5627e72
KimDongHyun <kdh2637@hanyang.ac.kr>
2019-05-30 01:30:52 +0900

    Refs #1 : Modify index.js to welcome a new chatter

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.get('/', function(req, res){
  
 io.on('connection', function(socket){
   console.log('a user connected');
+  io.emit('chat message', "Please Hi to new Chatter.");
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });

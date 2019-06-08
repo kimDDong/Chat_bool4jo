9ad9dd3cf5ee63e1e9cd37929dce86cbd5da3f6c
Junbeom Wi <minkwns@naver.com>
2019-06-09 02:05:57 +0900

    Modifying codes

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -7,23 +7,23 @@ $(function() {
 
     // login 에 대한 listening
     socket.on("login", function (data) {
-        $("#chatLog").append("<li style ='color : white; text-align:center;background-color:#63738a;;" +
-            "margin-left: -18px; margin-right: -18px;'><strong>『"+data+"』</strong> 님이 입장하셨습니다.</li>");
+        $("#chatLog").append("<li style ='color : white; text-align:center;background-color:darkgray;" +
+            "margin-left: 20px; margin-right: 20px;'><strong>『"+data+"』</strong> 님이 입장하셨습니다.</li>");
 
     });
     // logout 에 대한 listening
     socket.on("logout", function (data) {
-        $("#chatLog").append("<li style ='color : white;text-align:center;background-color:#63738a;margin-left: -18px; " +
-            "margin-right: -18px;'><strong>『"+data+"』</strong> 님이 퇴장하셨습니다.</li>");
+        $("#chatLog").append("<li style ='color : white;text-align:center;background-color:darkgray;margin-left: 20px; " +
+            "margin-right: 20px;'><strong>『"+data+"』</strong> 님이 퇴장하셨습니다.</li>");
     });
     // chat 에 대한 listening
     socket.on("chat", function (data) {
-    	if(username == data.username){
-	        $("#chatLog").append("<li style ='text-align:right;background-color:#ffb8c6;margin-left: -18px; margin-right: -18px; '><strong>"+"</strong>"+data.msg+"</li>");
-    	}
-    	else{
-	        $("#chatLog").append("<li style ='text-align:left;background-color:#f9c6cf;margin-left: -18px; margin-right: -18px; '><strong>"+data.username+"</strong> : "+data.msg+"</li>");
-    	}
+        if(username == data.username){
+            $("#chatLog").append("<li style ='text-align:right;'><strong>"+"나 : "+"</strong>"+data.msg+"</li>");
+        }
+        else{
+            $("#chatLog").append("<li style ='text-align:left;'><strong>"+data.username+"</strong> : "+data.msg+"</li>");
+        }
         $("#chatLog").scrollTop($("#chatLog")[0].scrollHeight);
     });
 

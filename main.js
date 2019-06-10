9effa58c59a9fcf56f359257fd1e8c91b5bb956f
Savzak <darkcruel12@naver.com>
2019-06-11 01:09:12 +0900

    change icon
    change icon

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let win
 
 function createWindow () {
   // 새로운 브라우저 창을 생성합니다.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width: 1200, height: 600, icon : __dirname + '/icon.png'})
 
   // 그리고 현재 디렉터리의 index.html을 로드합니다.
   win.loadURL('http://localhost:8000');
